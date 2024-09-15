f7bfe2df5e19b61d44999e19248e6b131ddd0d68
Marvin738 <121965130+Marvin738@users.noreply.github.com>
2024-09-14 23:54:34 -0600

    script.js

--- felicitacion_bren/script.js
+++ felicitacion_bren/script.js
@@ -1,7 +1,7 @@
 const alternatives = [
   {text:"", images:"images/cat-01.gif"},
   {text:"No se en que momento lo abriste pero espero ser el primero en felicitarte.", images:"images/cat-02.gif"},
-  {text:"Espero que durmieras bien, que te gustara la flor y tambien esta dinamica.", images:"images/cat-03.gif"},
+  {text:"Espero que durmieras bien (por si lo abriste en la mañana), que te gustara la flor y tambien esta dinamica.", images:"images/cat-03.gif"},
   {text:"solo espero que el dia de hoy te la pases de lo mejor y que te alegre el dia este pequeño detalle.", images:"images/cat-04.gif"},
   {text:"te quiero preciosa, me gustas mucho y bueno se que ya te lo he dicho pero quiero que sepas que no voy a dejar de recordartelo.", images:"images/cat-yes.gif"},
 ]
